c6c010624a54c37f069d3dc822067277918cff2c
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-12 17:13:22 -0700

    first commit

---
+++ SoloLevel/SoloLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloLevelApp.swift
+//  SoloLevel
+//
+//  Created by csuftitan on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SoloLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
