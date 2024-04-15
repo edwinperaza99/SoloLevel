18837260723e43b268def80f064b3807b7fb98b8
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-14 23:58:22 -0700

    add signout button

--- SoloLevel/TabBarViews/SettingsView.swift
+++ SoloLevel/TabBarViews/SettingsView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section("Challenges") {
+                    Text("Edit challenges")
+                }
+                Section("Player profile") {
+                    Text("Change username")
+                    Text("Change email")
+                    Text("Change password")
+                }
+                Section("Stats"){
+                    Text("Change job")
+                    Text("Change")
+                }
+                Section("Logout") {
+                    Button {
+                        AuthService.shared.signOut()
+                        print("Button to log out pressed")
+                    } label: {
+                        HStack{
+                            Image(systemName: "power")
+                                .foregroundStyle(.red)
+                            Text("Log out")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+            }
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
