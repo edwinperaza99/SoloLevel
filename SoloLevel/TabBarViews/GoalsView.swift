41cccb03f6189204f1c5a79d5a239f2c9ac2e3c3
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-25 22:55:07 -0700

    change struct for swift data

--- SoloLevel/TabBarViews/GoalsView.swift
+++ SoloLevel/TabBarViews/GoalsView.swift
@@ -5,6 +5,7 @@
 //  Created by Edwin on 4/13/24.
 //
 
+import SwiftData
 import SwiftUI
 
 //just for testing
@@ -15,20 +16,28 @@ import SwiftUI
 
 struct GoalsView: View {
     @Environment(\.modelContext) var modelContext
+    @Query(sort: [
+        SortDescriptor(\Goal.title),
+//        SortDescriptor(\Book.author)
+    ]) var goals: [Goal]
     
     @State private var showingAddScreen = false
     
 //    just for testing
-    @State private var goals = [
-           Goal(title: "Run a marathon"),
-           Goal(title: "Read 50 books this year"),
-           Goal(title: "Learn to play the piano"),
-           Goal(title: "Complete a SwiftUI course"),
-           Goal(title: "Save more money")
-       ]
+//    @State private var goals = [
+//           Goal(title: "Run a marathon"),
+//           Goal(title: "Read 50 books this year"),
+//           Goal(title: "Learn to play the piano"),
+//           Goal(title: "Complete a SwiftUI course"),
+//           Goal(title: "Save more money")
+//       ]
 
     func deleteGoal(at offsets: IndexSet) {
-           goals.remove(atOffsets: offsets)
+        for offset in offsets {
+            let goal = goals[offset]
+            modelContext.delete(goal)
+        }
+//           goals.remove(atOffsets: offsets)
        }
     
     var body: some View {
