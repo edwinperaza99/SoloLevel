2be332135174166272aefd6a61d172d69c7fa67a
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-05-03 13:58:45 -0700

    change edit challenges to work with swift data

--- SoloLevel/SettingsViews/EditChallengesView.swift
+++ SoloLevel/SettingsViews/EditChallengesView.swift
@@ -5,20 +5,83 @@
 //  Created by Edwin on 4/24/24.
 //
 
+import SwiftData
 import SwiftUI
 
-struct EditChallengesView: View {
-    let challenges = ["Challenge 1", "Challenge 2", "Challenge 3", "Challenge 4"]
+struct ChallengesList: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var challenges: [Challenge]
 
+    func deleteChallenge(at offsets: IndexSet) {
+        for offset in offsets {
+            let challenge = challenges[offset]
+            modelContext.delete(challenge)
+        }
+    }
     var body: some View {
         List {
-            ForEach(challenges, id: \.self) { challenge in
-                NavigationLink(destination: EditChallengeView(challengeTitle: challenge)) {
-                    Text(challenge)
+           ForEach(challenges) { challenge in
+               NavigationLink(value: challenge){
+                  Text(challenge.text)
+                      .font(.headline)
+               }
+           }
+           .onDelete(perform: deleteChallenge)
+       }
+    }
+    init(sortOrder: [SortDescriptor<Challenge>]) {
+           _challenges = Query(sort: sortOrder)
+       }
+}
+
+struct EditChallengesView: View {
+    @State private var sortOrder = [
+           SortDescriptor(\Challenge.text),
+           SortDescriptor(\Challenge.quantity),
+       ]
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationStack {
+            ChallengesList(sortOrder: sortOrder)
+            .navigationTitle("Edit Challenges")
+            .navigationDestination(for: Challenge.self) { challenge in
+               EditChallengeView(challenge: challenge)
+           }
+            .scrollBounceBehavior(.basedOnSize)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
                 }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Sort by Text")
+                                .tag([
+                                    SortDescriptor(\Challenge.text),
+                                    SortDescriptor(\Challenge.quantity),
+                                ])
+                            Text("Sort by Repetitions")
+                                .tag([
+                                    SortDescriptor(\Challenge.quantity),
+                                    SortDescriptor(\Challenge.text)
+                                ])
+                        }
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        showingAddScreen.toggle()
+                    }) {
+                        Label("Add Challenge", systemImage: "plus")
+                    }
+                }
+         
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddChallengeView()
             }
         }
-        .navigationTitle("Edit Challenges")
         .preferredColorScheme(.dark)
     }
 }
