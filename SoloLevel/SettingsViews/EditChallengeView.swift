adafca7b35e788f8355041aeb08bef09499741d9
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-05-03 14:23:22 -0700

    make view to edit challenge

--- SoloLevel/SettingsViews/EditChallengeView.swift
+++ SoloLevel/SettingsViews/EditChallengeView.swift
@@ -5,28 +5,59 @@
 //  Created by Edwin on 4/24/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct EditChallengeView: View {
-    @State var challengeTitle: String
-    @State var repetitions: Int = 0
-
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    @State private var showingDeleteAlert = false
+    
+    @Bindable var challenge: Challenge
+    
     var body: some View {
         Form {
-            Section(header: Text("Challenge Title")) {
-                TextField("Title", text: $challengeTitle)
+            Section("Challenge:") {
+                TextField(challenge.text, text: $challenge.text)
             }
-            Section(header: Text("Repetitions")) {
-                Stepper(value: $repetitions, in: 0...100) {
-                    Text("\(repetitions) repetitions")
-                }
+            Section("Repetitions/quantity:"){
+                TextField("\(challenge.quantity)", value: $challenge.quantity, formatter: NumberFormatter())
             }
+//            Section("Measurement:"){
+//                TextField(challenge.measure, text: $challenge.measure)
+//            }
         }
         .navigationTitle("Edit Challenge")
+//        .navigationBarTitleDisplayMode(.inline)
         .preferredColorScheme(.dark)
+        .scrollBounceBehavior(.basedOnSize)
+        .alert("Delete challenge", isPresented: $showingDeleteAlert) {
+              Button("Delete", role: .destructive, action: deleteChallenge)
+              Button("Cancel", role: .cancel) { }
+          } message: {
+              Text("Are you sure?")
+          }
+        .toolbar {
+            Button("Delete this challenge", systemImage: "trash") {
+                showingDeleteAlert = true
+            }
+        }
     }
+    func deleteChallenge() {
+           modelContext.delete(challenge)
+           dismiss()
+       }
 }
 
 #Preview {
-    EditChallengeView(challengeTitle: "Challenge")
+    do {
+       let config = ModelConfiguration(isStoredInMemoryOnly: true)
+       let container = try ModelContainer(for: Goal.self, configurations: config)
+        let example = Challenge(text: "Test challenge", quantity: 20)
+
+       return EditChallengeView(challenge: example)
+           .modelContainer(container)
+  } catch {
+       return Text("Failed to create preview: \(error.localizedDescription)")
+  }
 }
