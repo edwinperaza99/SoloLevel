ebceb4faf96374b4f5f6050fde1cf374feefc319
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-05-03 01:18:55 -0700

    allow user to sort in different ways

--- SoloLevel/TabBarViews/GoalsView.swift
+++ SoloLevel/TabBarViews/GoalsView.swift
@@ -8,69 +8,16 @@
 import SwiftData
 import SwiftUI
 
-
-struct GoalsView: View {
+struct GoalsList: View {
     @Environment(\.modelContext) var modelContext
-    @Query(sort: [
-        SortDescriptor(\Goal.title),
-//        SortDescriptor(\Goal.description)
-    ]) var goals: [Goal]
+    @Query var goals: [Goal]
     
-    @State private var showingAddScreen = false
-
     func deleteGoal(at offsets: IndexSet) {
         for offset in offsets {
             let goal = goals[offset]
             modelContext.delete(goal)
         }
     }
-    
-    var body: some View {
-        NavigationStack {
-            List {
-                ForEach(goals) { goal in
-                    NavigationLink(value: goal){
-                        HStack {
-                           VStack(alignment: .leading, spacing: 4) {
-                               Text(goal.title)
-                                   .font(.headline)
-                               Text(goal.statusDescription)
-                                   .font(.subheadline)
-                                   .foregroundColor(colorForStatus(goal.statusDescription))
-                           }
-                           Spacer()
-                           Text(goal.dueDateDescription())
-                               .font(.subheadline)
-                               .foregroundColor(.secondary)
-                       }
-                    }
-                }
-                .onDelete(perform: deleteGoal)
-            }
-            .navigationTitle("Goals")
-            .navigationDestination(for: Goal.self) { goal in
-               GoalView(goal: goal)
-           }
-            .scrollBounceBehavior(.basedOnSize)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    EditButton()
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        showingAddScreen.toggle()
-                    }) {
-                        Label("Add Goal", systemImage: "plus")
-                    }
-                }
-            }
-            .sheet(isPresented: $showingAddScreen) {
-                AddGoalView()
-            }
-        }
-        .preferredColorScheme(.dark)
-    }
-    
     private func colorForStatus(_ status: String) -> Color {
             switch status {
             case "Completed":
@@ -83,6 +30,93 @@ struct GoalsView: View {
                 return .gray
             }
         }
+
+    
+    var body: some View {
+        List {
+            ForEach(goals) { goal in
+                NavigationLink(value: goal){
+                    HStack {
+                       VStack(alignment: .leading, spacing: 4) {
+                           Text(goal.title)
+                               .font(.headline)
+                           Text(goal.statusDescription)
+                               .font(.subheadline)
+                               .foregroundColor(colorForStatus(goal.statusDescription))
+                       }
+                       Spacer()
+                       Text(goal.dueDateDescription())
+                           .font(.subheadline)
+                           .foregroundColor(.secondary)
+                   }
+                }
+            }
+            .onDelete(perform: deleteGoal)
+        }
+    }
+    init(sortOrder: [SortDescriptor<Goal>]) {
+        _goals = Query(sort: sortOrder)
+    }
+}
+
+struct GoalsView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var sortOrder = [
+        SortDescriptor(\Goal.dueDate),
+        SortDescriptor(\Goal.title),
+    ]
+    
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationStack {
+            GoalsList(sortOrder: sortOrder)
+            .navigationTitle("Goals")
+            .navigationDestination(for: Goal.self) { goal in
+               GoalView(goal: goal)
+           }
+            .scrollBounceBehavior(.basedOnSize)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Sort by Title")
+                                .tag([
+                                    SortDescriptor(\Goal.title),
+                                    SortDescriptor(\Goal.dueDate),
+                                ])
+                            Text("Sort by Due Date")
+                                .tag([
+                                    SortDescriptor(\Goal.dueDate),
+                                    SortDescriptor(\Goal.title)
+                                ])
+                            Text("Sort by Creation Date")
+                                .tag([
+                                    SortDescriptor(\Goal.dateCreated),
+                                    SortDescriptor(\Goal.title)
+                                ])
+                        }
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        showingAddScreen.toggle()
+                    }) {
+                        Label("Add Goal", systemImage: "plus")
+                    }
+                }
+         
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddGoalView()
+            }
+        }
+        .preferredColorScheme(.dark)
+    }
 }
 
 #Preview {
