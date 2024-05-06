cf645e37f3ccc55fb582d3f8308b5d095e83bfad
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-05-06 11:42:23 -0700

    create about view

---
+++ SoloLevel/components/AboutView.swift
@@ -0,0 +1,65 @@
+//
+//  AboutView.swift
+//  SoloLevel
+//
+//  Created by csuftitan on 5/6/24.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                // App Information
+                VStack(spacing: 0) {
+                    RectangleDivider()
+                    Text("About Solo Level")
+                    .font(.largeTitle)
+                    .bold()
+                    .textCase(.uppercase)
+                    RectangleDivider()
+                }
+                
+                Text("Solo Level is a self-improvement app inspired by the themes of progression and personal growth, akin to the journey of Sung Jinwoo in the popular manwha and anime series *Solo Leveling*. The app integrates a gamified level system where users can set daily challenges and earn experience points upon completion, with a focus on fostering consistency and discipline in personal goals.")
+                    .font(.body)
+                
+                // Features Section
+                Text("Features")
+                    .font(.title)
+                    .bold()
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    FeatureItemView(title: "Challenges", description: "Set and track daily personal challenges in various categories such as fitness, reading, or skill development.")
+                    FeatureItemView(title: "Leveling System", description: "Gain experience points and level up by consistently meeting your daily goals.")
+                    FeatureItemView(title: "Progress Tracking", description: "Visualize your progress with statistics and level progression to keep track of your improvement over time.")
+                    FeatureItemView(title: "User Profile", description: "Personalize your experience with a customizable profile, including a profile picture and username.")
+                }
+                .padding(.leading, 10)
+                
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+struct FeatureItemView: View {
+    let title: String
+    let description: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.headline)
+                .bold()
+            Text(description)
+                .font(.body)
+                .multilineTextAlignment(.leading)
+        }
+    }
+}
+
+#Preview {
+    AboutView()
+}
